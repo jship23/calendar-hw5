d9a78cc56c32662ec704e2766ca4fa19bde6d279
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-15 16:57:36 -0600

    coded current time

---
+++ script.js
@@ -0,0 +1,12 @@
+//get and display current time
+$("#currentDay").text(moment().format("dddd, MMMM Do h:mm A"));
+
+//check for current time
+
+//if hour is past
+
+//if hour is current
+
+//if hour is future
+
+//store text that is saved
