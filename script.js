82fbb828c8a57c8b891783c5bc23ec176f77e6d5
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-10-17 14:39:06 -0600

    udated if else

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
+//get and display current date & time
+var currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do h:mm A"));
 
-//get and display current time
-$("#currentDay").text(moment().format("dddd, MMMM Do h:mm A"));
 
 //store text that is saved
 $(".saveBtn").on("click", function(){
     //get values from textarea
     var value = $(this).siblings(".description").val();
-  
+    var saveTime = $(this).parent().attr("id")
     //save to local storage
-    localStorage.setItem("Value", value);
+    localStorage.setItem("storedValue", value, "savedTime", saveTime);
     //get value from storage
-    localStorage.getItem("Value");
-   
+    localStorage.getItem("storedValue");
+    console.log(value, saveTime)
 })
 
 
-//if hour is past
 
-//if hour is current
+function updateHour() {
+    //current hour
+    var currentHour = moment().hours();
+    //loop over each time block
+    $(".time-block").each(function() {
+        var hourBlock = parseInt($(this).attr("id"))
 
-//if hour is future
+        //if hour is past
+        if(hourBlock < currentHour){
+            $(this).addClass("past")
+        }
+        //if hour is current
+        else if(hourBlock === currentHour) {
+            $(this).addClass("present")
+        }
+        //if hour is future
+        else {
+            $(this).addClass("future")
+        }
+    });
+}
+updateHour();
+
 
